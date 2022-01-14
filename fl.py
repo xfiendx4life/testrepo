481dc75c90dcdc943334696f1072f4ae5d0e9fad
Kirill <xfiendx4life@gmail.com>
2022-01-14 13:40:39 +0300

    Fixes

--- fl.py
+++ fl.py
@@ -1,16 +1,17 @@
-from flask import Flask, redirect, render_template, escape, url_for, abort
+from flask import Flask, render_template, escape, abort
 
 app = Flask(__name__)
 # app.register_error_handler('404.html', page_not_found)
 
 class Items:
-    def __init__(self, name='item', descr='some description', price='300$') -> None:
+    def __init__(self, name='item', descr='some description', price='300$', img="123.png") -> None:
         self.name = name
         self.descr = descr
         self.price = price
+        self.image = img
 
 ITEMS = [Items('Alexei', 'Kaput', '1945'), Items(),\
-    Items('panther', "the most beautiful panther eva", "400$")]
+    Items('panther', "the most beautiful panther eva", "400$", "shitty_panther_ytsv-p3.png")]
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -31,6 +32,7 @@ def catalog():
 @app.route('/item/<item>')
 def show_item_profile(item):
     # show the user profile for that user
+    item = escape(item)
     for thing in ITEMS:
         if thing.name == item:
             return render_template('item.html', item=thing)
