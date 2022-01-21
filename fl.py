edf4cb18be75de42088b876120c4c216bacce0a8
Kirill <xfiendx4life@gmail.com>
2022-01-21 15:22:05 +0300

    + New Template structure + Requests proceeding + fixed styles

--- fl.py
+++ fl.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template, escape, abort
+from crypt import methods
+from flask import Flask, render_template, escape, abort, request
 
 app = Flask(__name__)
-# app.register_error_handler('404.html', page_not_found)
+# //app.register_error_handler('404.html', page_not_found)
 
 class Items:
-    def __init__(self, name='item', descr='some description', price='300$', img="123.png") -> None:
+    def __init__(self, name='item', descr='some description', price=300, img="123.png") -> None:
         self.name = name
         self.descr = descr
         self.price = price
         self.image = img
 
+class Feedback:
+    def __init__(self, login, text):
+        self.login = login
+        self.text = text
+
 ITEMS = [Items('Alexei', 'Kaput', '1945'), Items(),\
-    Items('panther', "the most beautiful panther eva", "400$", "shitty_panther_ytsv-p3.png")]
+    Items('panther', "the most beautiful panther eva", 400, "shitty_panther_ytsv-p3.png"), Items(), Items(), Items(), Items(), Items(), Items()]
+FEEDBACK = []
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -21,13 +28,21 @@ def page_not_found(e):
 def index():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about():
-    return render_template('about.html')
+    if request.method == 'POST':
+        login = request.form.get('login')
+        text = request.form.get('text')
+        f = Feedback(login, text)
+        FEEDBACK.append(f)
+    return render_template('about.html', feedback=FEEDBACK)
 
 @app.route('/catalog')
 def catalog():
-    return render_template('catalog.html', items=ITEMS)
+    min_price = int(request.args.get('min_price', 0))
+    max_price = int(request.args.get('max_price', 10e9))
+    items = list(filter(lambda x: min_price <= int(x.price) <= max_price, ITEMS))
+    return render_template('catalog.html', items=items)
 
 @app.route('/item/<item>')
 def show_item_profile(item):
