5a3f53a2f9038f6364a90b4117b5d1b94b58960d
xfiendx4life <xfiendx4life@gmail.com>
2022-02-06 16:00:29 +0300

    Deleted db folder

--- db/ORM/p.py
+++
@@ -1,8 +0,0 @@
-from service import *
-
-s = createStudent("punk", "dirty")
-c = addCourse("sqLAlchemy", 4)
-st = addStreams(45, c)
-db.session.commit()
-addGrades(5, s, st)
-db.session.commit()
--- db/test.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO grades (student_id, stream_id, grade) VALUES
-	(3, 2, 5),
-	(3, 1, 4.5),
-	(3, 3, 3);
